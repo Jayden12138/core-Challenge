2aa2948f096c79d95148d0f673fc3de1565fc89d
Jayden <18993882884@163.com>
2024-05-17 15:42:20 +0800

    feat(reactivity): effect scheduler

--- packages/reactivity/src/effect.ts
+++ packages/reactivity/src/effect.ts
@@ -1,6 +1,6 @@
 class ActiveEffect {
 	private _fn
-	constructor(func) {
+	constructor(func, public scheduler?) {
 		this._fn = func
 	}
 
@@ -34,11 +34,17 @@ export function trigger(target, key) {
 	let depsMap = targetMap.get(target)
 	let deps = depsMap.get(key)
 
-	deps.forEach(effect => effect.run())
+	deps.forEach(effect => {
+		if (effect.scheduler) {
+			effect.scheduler()
+		} else {
+			effect.run()
+		}
+	})
 }
 
-export function effect(func) {
-	const _effect = new ActiveEffect(func)
+export function effect(func, options: any = {}) {
+	const _effect = new ActiveEffect(func, options.scheduler)
 	_effect.run()
 
 	return _effect.run.bind(_effect)
