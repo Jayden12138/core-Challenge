186819ab510caa82868d159e02308f519d4087b0
Jayden <18993882884@163.com>
2024-05-17 15:33:57 +0800

    case: effect scheduler

--- global.d.ts
+++ global.d.ts
@@ -1,7 +1,8 @@
-import { it, expect, describe } from 'bun:test'
+import { it, expect, describe, jest } from 'bun:test'
 
 declare global {
 	var it: typeof it
 	var expect: typeof expect
 	var describe: typeof describe
+	var jest: typeof jest
 }
