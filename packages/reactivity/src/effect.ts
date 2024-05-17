e6d8a36ce7e57fac90306ac311351fbd130ae3a9
Jayden <18993882884@163.com>
2024-05-17 11:42:50 +0800

    feat(reactivity): effect & reactive happy path

--- packages/reactivity/src/effect.ts
+++ packages/reactivity/src/effect.ts
@@ -1,1 +1,41 @@
-export function effect() {}
+class ActiveEffect {
+	private _fn
+	constructor(func) {
+		this._fn = func
+	}
+
+	run() {
+		activeEffect = this
+
+		this._fn()
+	}
+}
+
+const targetMap = new Map()
+let activeEffect
+export function track(target, key) {
+	let depsMap = targetMap.get(target)
+	if (!depsMap) {
+		targetMap.set(target, (depsMap = new Map()))
+	}
+
+	let deps = depsMap.get(key)
+	if (!deps) {
+		depsMap.set(key, (deps = new Set()))
+	}
+
+	if (!deps.has(activeEffect)) {
+		deps.add(activeEffect)
+	}
+}
+export function trigger(target, key) {
+	let depsMap = targetMap.get(target)
+	let deps = depsMap.get(key)
+
+	deps.forEach(effect => effect.run())
+}
+
+export function effect(func) {
+	const _effect = new ActiveEffect(func)
+	_effect.run()
+}
--- packages/reactivity/src/reactive.ts
+++ packages/reactivity/src/reactive.ts
@@ -1,1 +1,21 @@
-export function reactive() {}
+import { track, trigger } from './index'
+
+export function reactive(raw) {
+	return new Proxy(raw, {
+		get(target, key) {
+			const res = Reflect.get(target, key)
+
+			track(target, key)
+
+			return res
+		},
+
+		set(target, key, value) {
+			let res = Reflect.set(target, key, value)
+
+			trigger(target, key)
+
+			return res
+		},
+	})
+}
