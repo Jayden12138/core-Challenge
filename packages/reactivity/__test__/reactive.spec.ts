5354195ea07f52dd681afd50b7b9a2ab2c4eae26
Jayden <18993882884@163.com>
2024-05-20 15:49:33 +0800

    case: isProxy

--- packages/reactivity/__test__/reactive.spec.ts
+++ packages/reactivity/__test__/reactive.spec.ts
@@ -43,4 +43,20 @@ describe('reactive', () => {
 		expect(isReadonly(observed.bar)).toBe(true)
 		expect(isReadonly(original.bar)).toBe(false)
 	})
+
+	it('isProxy happy path reactive', () => {
+		const original = { foo: 1, bar: { baz: 2 } }
+
+		const observed = readonly(original)
+
+		expect(isProxy(observed)).toBe(true)
+	})
+
+	it('isProxy happy path readonly', () => {
+		const original = { foo: 1, bar: { baz: 2 } }
+
+		const observed = reactive(original)
+
+		expect(isProxy(observed)).toBe(true)
+	})
 })
