d86bc80811ce442fa07ffb1aeb70a275ee9ae0f4
Jayden <18993882884@163.com>
2024-05-20 14:57:34 +0800

    case: nested object reactive & readonly

--- packages/reactivity/__test__/reactive.spec.ts
+++ packages/reactivity/__test__/reactive.spec.ts
@@ -14,4 +14,31 @@ describe('reactive', () => {
 		expect(isReadonly(observed)).toBe(true)
 		expect(isReadonly(original)).toBe(false)
 	})
+
+	it('nested object reactive', () => {
+		const original = {
+			nested: {
+				foo: 1,
+			},
+			array: [{ bar: 2 }],
+		}
+		const observed = reactive(original)
+
+		// 嵌套属性
+		expect(isReactive(observed.nested)).toBe(true)
+		expect(isReactive(observed.array)).toBe(true)
+		expect(isReactive(observed.array[0])).toBe(true)
+	})
+
+	it('nested object readonly', () => {
+		const original = { foo: 1, bar: { baz: 2 } }
+		const observed = readonly(original)
+
+		expect(isReadonly(observed)).toBe(true)
+		expect(isReadonly(original)).toBe(false)
+
+		// 嵌套属性
+		expect(isReadonly(observed.bar)).toBe(true)
+		expect(isReadonly(original.bar)).toBe(false)
+	})
 })
